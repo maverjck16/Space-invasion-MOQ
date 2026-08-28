6989501c5d50f31c81c95f359cf3504ae72b2246
maverjck16 <frabrini04@gmail.com>
2026-08-28 17:25:33 +0200

    Aggiorna config

--- TS/frontend/src/config.ts
+++ TS/frontend/src/config.ts
@@ -1,6 +1,6 @@
 //definisco costanti globali
-export const APP_PREFIX = "moq-keycast"; //prefisso di tutti i path MOQ, nel publisher uso
-// const path = `${APP_PREFIX}/${room}/${username}`, esempio di path: moq-keycast/general/francesco
+export const APP_PREFIX = "space-invasion"; //prefisso di tutti i path MOQ, nel publisher uso
+// const path = `${APP_PREFIX}/${room}/${username}`, esempio di path: space-invasion/general/francesco
 export const TRACK_GAME = "game"; //nome della track che uso per inviare i dati di digitazione  
 export const RELAY_URL = "https://spaceinvasion.ddns.net:4443"; //url del relay, in questo caso è in locale
 // export const RELAY_URL = "https://cdn.moq.dev"; //url del relay in remoto
